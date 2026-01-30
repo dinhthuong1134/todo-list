dcb6d848590434979a7aa35cd55087a17732102a
Phạm Đình Thương <thuonglop94dhp@gmail.com>
2026-01-30 17:14:19 +0700

    hehe

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,19 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
-// Tạo biến global để cache instance của NestJS
-let cachedServer: any;
+const server = express() as any;
 
 async function bootstrap() {
-  if (!cachedServer) {
-    const server = express();
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(server),
-    );
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
-    app.enableCors();
-    await app.init();
-    
-    // Lưu instance vào cache để dùng cho lần sau
-    cachedServer = server;
-  }
-  return cachedServer;
+  app.enableCors();
+
+  await app.listen(3000);
 }
 
-// Export duy nhất một function cho Vercel
-export default async (req: any, res: any) => {
-  const instance = await bootstrap();
-  return instance(req, res);
-};
+bootstrap();
+
+export default server;
